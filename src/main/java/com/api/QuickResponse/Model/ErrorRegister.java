bc2f241b4da037172d05b80d931abdc26efeee9f
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2022-06-06 11:46:55 +0700

    Add dependency gson to pom.xml

---
+++ src/main/java/com/api/QuickResponse/Model/ErrorRegister.java
@@ -0,0 +1,45 @@
+package com.api.QuickResponse.Model;
+
+import javax.annotation.Generated;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class ErrorRegister {
+
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("errorCode")
+    @Expose
+    private Integer errorCode;
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+}
